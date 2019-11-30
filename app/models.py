e09756db902393610fe9689262d71ce38a742569
Shmizzy <carnold1@ithaca.edu>
2019-11-30 00:32:59 -0500

    Database Overhaul I

--- app/models.py
+++ app/models.py
@@ -4,18 +4,25 @@ from flask_login import UserMixin
 from app import login
 
 
-class User (UserMixin, db.Model):
+# Each User can have multiple comments/ratings, but each rating/comment can only have one User
+# Each Professor can receive multiple comments/ratings, but each rating/comment can only have one Professor
+
+
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     first_name = db.Column(db.String(64), index=True)
     last_name = db.Column(db.String(64), index=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    ratings = db.relationship('Rating', backref='author', lazy='dynamic')
+    ratings = db.relationship('Rating', backref='user', lazy='dynamic')
+
+    # ratings creates an SQLAlchemy Object that allows for queries to be made
+    # backref='user' allows for us to type Rating.user.first_name, and access those values
+    # list is accessed with User.ratings.all()
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
-
 
 
 @login.user_loader
@@ -23,42 +30,56 @@ def load_user(id):
     return User.query.get(int(id))
 
 
+class Professor(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(64), index=True)
+    last_name = db.Column(db.String(64), index=True)
+    ratings = db.relationship('Rating', backref='professor', lazy='dynamic')
+    courses = db.relationship('ProfessorToCourse', backref='professor', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Professor {}>'.format(self.last_name)
+
+
 class Rating(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    section_id = db.Column(db.Integer, db.ForeignKey("Section", "id"))
-    section = db.relationship('Section', backref='ratings', lazy='dynamic')
+    rating = db.Column(db.Integer, index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    professor_id = db.Column(db.Integer, db.ForeignKey('professor.id'))
 
     def __repr__(self):
         return '<Rating {}>'.format(self.id)
 
 
-class Professor(db.Model):
+class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(64), index=True)
-    last_name = db.Column(db.String(64), index=True)
-    average_rating = db.Column(db.Float, index=True)
+    comment = db.Column(db.String(400), index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    professor_id = db.Column(db.Integer, db.ForeignKey('professor.id'))
 
     def __repr__(self):
-        return '<Professor {}>'.format(self.last_name)
+        return '<Comment {}>'.format(self.id)
+
+
+# Each User can have multiple courses, each course can have multiple users
+# Each Professor can have multiple courses, each course can have multiple professors
+# ProfessorToCourse class required to mitigate that relationship
 
 
 class Course(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True)
     description = db.Column(db.String(300), index=True)
+    professors = db.relationship('ProfessorToCourse', backref='course', lazy='dynamic')
 
     def __repr__(self):
         return '<Course {}>'.format(self.name)
 
 
-class Section(db.Model):
+class ProfessorToCourse(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    course_id = db.relationship(db.Integer, db.ForeignKey)  # insert not null
-    professor_id = db.relationship(db.Integer, db.ForeignKey)
-    semester = db.Column(db.String(64), index=True)
-    year = db.Column(db.String(64), index=True)
+    professor_id = db.Column(db.Integer, db.ForeignKey('professor.id'))
+    course_id = db.Column(db.Integer, db.ForeignKey('professor.id'))
 
     def __repr__(self):
-        return '<Professor {}>'.format(self.id)
-
-
+        return '<ProfessorToCourse {}>'.format(self.name)
