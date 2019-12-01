d9127cf524e38c7b7263e9b7038042d9f16edd2c
Shmizzy <carnold1@ithaca.edu>
2019-12-01 17:27:08 -0500

    NAVBAR implemented I

--- app/routes.py
+++ app/routes.py
@@ -13,17 +13,13 @@ def index():
     return render_template('index.html', title='Home', user=user)
 
 
-@app.route('/about')
+@app.route('/about_us')
 def about_us():
-    mission_statement = {"Our mission is to create a more efficient and intuitive application of RateMyProfessor’s "
-                         "framework by utilizing key concepts of design and databases, specifically for Ithaca "
-                         "College students. "
-                         " RateMyProfessor uses a professor-strictly build,"
-                         " whereas our website is going to take a "
-                         "more intuitive approach that allows for organization by courses, and ratings of professors "
-                         "that pertain to specific courses they teach."}
+    mission_statement = '''Our mission is to create a more efficient and intuitive application of RateMyProfessor’s
+                         framework by utilizing key concepts of design and databases, specifically for Ithaca
+                         College students.'''
 
-    return render_template('about_us.html', title='About Us', paragraph=mission_statement)
+    return render_template('about_us.html', title='About Us', mission_statement=mission_statement)
 
 
 @app.route('/reset_db')
